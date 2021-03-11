de5ad5d4b0762d5fbd47dff8741bb505174a4317
XanderQue <alfredq11@live.com>
2021-03-11 10:54:10 -0800

    Added some lines to collect a string(Name) and an int(Favorite Number) from input on console.

--- PetProjects/Program.cs
+++ PetProjects/Program.cs
@@ -4,10 +4,26 @@ namespace PetProjects
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            string name = "no Name";
+            int favNum = 0;
+
             Console.WriteLine("Hello World!");
-            Console.ReadLine(); // create a pause, this stops the app from auto ending.
+
+            Console.WriteLine("Lets get a string, type your name : ");
+            name = Console.ReadLine(); // get input string
+
+            Console.WriteLine("Hi " + name);
+
+            Console.WriteLine("Whats your favorite integer? : ");
+            favNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(favNum + " is a pretty cool number after all, not surprised.");
+
+            Console.WriteLine("Bye!");
+
         }
     }
 }
