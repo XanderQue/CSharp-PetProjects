efc8b55883fdc136f51e3a668ad4247bbd4f8b96
XanderQue <alfredq11@live.com>
2021-03-11 10:45:45 -0800

    Add project files.

---
+++ PetProjects/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PetProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); // create a pause, this stops the app from auto ending.
+        }
+    }
+}
